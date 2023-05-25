d21603550db690c93c3c8b92455c11679518153c
Samuel Dare <sam@tunnelvisionlabs.xyz>
2023-05-25 11:25:56 +0000

    chore: change name

--- k8s-deploy/k8s/axon/deploy.sh
+++ k8s-deploy/k8s/axon/deploy.sh
@@ -4,26 +4,26 @@ function clean() {
     set +e
     echo "DEBUG" "Clean env to delete old axons, please wait..."
 
-    (kubectl get sts -n "axon" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^axon" || true) | while read -r name; do
-        kubectl delete sts "$name" -n "axon">/dev/null 2>&1
+    (kubectl get sts -n "khalani-testnet" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^axon" || true) | while read -r name; do
+        kubectl delete sts "$name" -n "khalani-testnet">/dev/null 2>&1
     done
-    (kubectl get pvc -n "axon" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^data" || true) | while read -r name; do
-        kubectl delete pvc "$name" -n "axon">/dev/null 2>&1
+    (kubectl get pvc -n "khalani-testnet" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^data" || true) | while read -r name; do
+        kubectl delete pvc "$name" -n "khalani-testnet">/dev/null 2>&1
     done
-    (kubectl get cm -n "axon" -o json | jq --raw-output '.items[].metadata.name' | grep -v "^kube" || true) | while read -r name; do
-        kubectl delete cm "$name" -n "axon">/dev/null 2>&1
+    (kubectl get cm -n "khalani-testnet" -o json | jq --raw-output '.items[].metadata.name' | grep -v "^kube" || true) | while read -r name; do
+        kubectl delete cm "$name" -n "khalani-testnet">/dev/null 2>&1
     done
 }
 
 function create_configmap() {
     echo "DEBUG" "create configmap for axons, please wait..."
     kubectl create namespace axon --dry-run=client -o yaml | kubectl apply -f -
-    kubectl create configmap node1-toml --from-file=./axon-config/node_1.toml -n axon
-    kubectl create configmap node2-toml --from-file=./axon-config/node_2.toml -n axon
-    kubectl create configmap node3-toml --from-file=./axon-config/node_3.toml -n axon
-    kubectl create configmap node4-toml --from-file=./axon-config/node_4.toml -n axon
-    kubectl create configmap genesis --from-file=./axon-config/genesis.json -n axon
-    kubectl create configmap db-options --from-file=./axon-config/default.db-options -n axon
+    kubectl create configmap node1-toml --from-file=./axon-config/node_1.toml -n khalani-testnet
+    kubectl create configmap node2-toml --from-file=./axon-config/node_2.toml -n khalani-testnet
+    kubectl create configmap node3-toml --from-file=./axon-config/node_3.toml -n khalani-testnet
+    kubectl create configmap node4-toml --from-file=./axon-config/node_4.toml -n khalani-testnet
+    kubectl create configmap genesis --from-file=./axon-config/genesis.json -n khalani-testnet
+    kubectl create configmap db-options --from-file=./axon-config/default.db-options -n khalani-testnet
 }
 function deploy_axon(){
     echo "DEBUG" "deploy axons, please wait..."
@@ -39,7 +39,7 @@ function deploy_axon(){
     kubectl apply -f ../pv/axon-logs-pvc.yaml
     echo "DEBUG" "waiting for axons running..."
     sleep 120
-    axon_running_num=`kubectl get pod -n axon | grep -i "axon" |grep -ci "running"`
+    axon_running_num=`kubectl get pod -n khalani-testnet | grep -i "axon" |grep -ci "running"`
     if [[ $axon_running_num -lt 4 ]]; then
         echo "axons not stable,please have a check"
     else
