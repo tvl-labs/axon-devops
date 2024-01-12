3e06b43679be917d21921b3a114be9d64fb1cea3
Samuel Dare <sam@tunnelvisionlabs.xyz>
2024-01-12 13:40:38 +0000

    fix: config script

--- k8s-deploy/k8s/axon/axon-config/generate_config.sh
+++ k8s-deploy/k8s/axon/axon-config/generate_config.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -ex
 
+# generate keys.json
+axon generate-keypair -n 4 -p . > keys.json
+
 # Update chain-spec.toml
 # Replace bls_pub_key, pub_key, and address with values from keys.json
 # Replace verifier_list with the updated values
-
-# Update verifier_list in chain-spec.toml
-# Example: Replace verifier_list with new values
-# Loop through each index and replace the values
 for ((i=0; i<4; i++)); do
     bls_pub_key=$(jq -r ".keypairs[$i].bls_public_key" keys.json)
     pub_key=$(jq -r ".keypairs[$i].public_key" keys.json)
@@ -17,10 +16,14 @@ for ((i=0; i<4; i++)); do
     sed -i "s/address = \"null\"/address = \"$address\"/g" chain-spec.toml
 done
 
-# Update multi address in node_n.toml files
-# Example: Replace multi_address with peer ids from keys.json
-# Use keys at index 0 for node 1, index 1 for node 2, and so on
+
+
+# Update multi_address in node_n.toml files
+# Replace multi_address with peer ids from keys.json
+# Use keys at index 0 for the first occurrence, index 1 for the second occurrence, and so on
 for ((i=1; i<=4; i++)); do
-    peer_id=$(jq -r ".keypairs[$((i-1))].peer_id" keys.json)
-    sed -i "s/multi_address = \"\/dns4\/axon-$i\/tcp\/8001\/p2p\/[^\"]*\"/multi_address = \"\/dns4\/axon-$i\/tcp\/8001\/p2p\/$peer_id\"/g" node_$i.toml
+    for ((j=0; j<4; j++)); do
+        peer_id=$(jq -r ".keypairs[$j].peer_id" keys.json)
+        sed -i "s|multi_address = \"/dns4/axon-$i/tcp/8001/p2p/[^\\\"]*\"|multi_address = \"/dns4/axon-$i/tcp/8001/p2p/$peer_id\"|g" node_$i.toml
+    done
 done
