31b368afaba0d5fe4af08517a721765079e649ce
Samuel Dare <sam@tunnelvisionlabs.xyz>
2024-01-12 11:50:49 +0000

    chore: fix script and configs

--- k8s-deploy/k8s/axon/axon-config/generate_config.sh
+++ k8s-deploy/k8s/axon/axon-config/generate_config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 # Update chain-spec.toml
 # Replace bls_pub_key, pub_key, and address with values from keys.json
@@ -7,19 +8,19 @@
 # Update verifier_list in chain-spec.toml
 # Example: Replace verifier_list with new values
 # Loop through each index and replace the values
-for ((i=0; i<N; i++)); do
+for ((i=0; i<4; i++)); do
     bls_pub_key=$(jq -r ".keypairs[$i].bls_public_key" keys.json)
     pub_key=$(jq -r ".keypairs[$i].public_key" keys.json)
     address=$(jq -r ".keypairs[$i].address" keys.json)
-    sed -i "s/bls_pub_key = \"null\"/bls_pub_key = \"$bls_pub_key\"/g" k8s-deploy/k8s/axon/axon-config/chain-spec.toml
-    sed -i "s/pub_key = \"null\"/pub_key = \"$pub_key\"/g" k8s-deploy/k8s/axon/axon-config/chain-spec.toml
-    sed -i "s/address = \"null\"/address = \"$address\"/g" k8s-deploy/k8s/axon/axon-config/chain-spec.toml
+    sed -i "s/bls_pub_key = \"null\"/bls_pub_key = \"$bls_pub_key\"/g" chain-spec.toml
+    sed -i "s/pub_key = \"null\"/pub_key = \"$pub_key\"/g" chain-spec.toml
+    sed -i "s/address = \"null\"/address = \"$address\"/g" chain-spec.toml
 done
 
 # Update multi address in node_n.toml files
 # Example: Replace multi_address with peer ids from keys.json
 # Use keys at index 0 for node 1, index 1 for node 2, and so on
-for ((i=1; i<=N; i++)); do
+for ((i=1; i<=4; i++)); do
     peer_id=$(jq -r ".keypairs[$((i-1))].peer_id" keys.json)
-    sed -i "s/multi_address = \"\/dns4\/axon-$i\/tcp\/8001\/p2p\/[^\"]*\"/multi_address = \"\/dns4\/axon-$i\/tcp\/8001\/p2p\/$peer_id\"/g" k8s-deploy/k8s/axon/axon-config/node_$i.toml
+    sed -i "s/multi_address = \"\/dns4\/axon-$i\/tcp\/8001\/p2p\/[^\"]*\"/multi_address = \"\/dns4\/axon-$i\/tcp\/8001\/p2p\/$peer_id\"/g" node_$i.toml
 done
