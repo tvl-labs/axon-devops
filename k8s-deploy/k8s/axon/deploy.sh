b7ed4684dba891967eb8d84a6b365cf66f6e4279
Samuel Dare <sam@tunnelvisionlabs.xyz>
2024-01-16 06:58:00 +0000

    feat: nodes running

--- k8s-deploy/k8s/axon/deploy.sh
+++ k8s-deploy/k8s/axon/deploy.sh
@@ -4,43 +4,43 @@ function clean() {
     set +e
     echo "DEBUG" "Clean env to delete old axons, please wait..."
     
-    (kubectl get sts -n "axon" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^axon" || true) | while read -r name; do
-        kubectl delete sts "$name" -n "axon">/dev/null 2>&1
+    (kubectl get sts -n "axon-trial" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^axon" || true) | while read -r name; do
+        kubectl delete sts "$name" -n "axon-trial">/dev/null 2>&1
     done
-    (kubectl get pvc -n "axon" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^data" || true) | while read -r name; do
-        kubectl delete pvc "$name" -n "axon">/dev/null 2>&1
+    (kubectl get pvc -n "axon-trial" -o json | jq --raw-output '.items[].metadata.name' | grep -E "^data" || true) | while read -r name; do
+        kubectl delete pvc "$name" -n "axon-trial">/dev/null 2>&1
     done
-    (kubectl get cm -n "axon" -o json | jq --raw-output '.items[].metadata.name' | grep -v "^kube" || true) | while read -r name; do
-        kubectl delete cm "$name" -n "axon">/dev/null 2>&1
+    (kubectl get cm -n "axon-trial" -o json | jq --raw-output '.items[].metadata.name' | grep -v "^kube" || true) | while read -r name; do
+        kubectl delete cm "$name" -n "axon-trial">/dev/null 2>&1
     done
-    kubectl delete namespace axon
+    kubectl delete namespace axon-trial
 }
 
 function create_configmap() {
     echo "DEBUG" "create configmap for axons, please wait..."
-    kubectl create namespace axon --dry-run=client -o yaml | kubectl apply -f -
-    kubectl create configmap chainspec --from-file=./axon-config/chain-spec.toml -n axon
-    kubectl create configmap net_0_key --from-file=./axon-config/net_0.key -n axon
-    kubectl create configmap net_1_key --from-file=./axon-config/net_1.key -n axon
-    kubectl create configmap net_2_key --from-file=./axon-config/net_2.key -n axon
-    kubectl create configmap net_3_key --from-file=./axon-config/net_3.key -n axon
-    kubectl create configmap bls_0_key --from-file=./axon-config/bls_0.key -n axon
-    kubectl create configmap bls_1_key --from-file=./axon-config/bls_1.key -n axon
-    kubectl create configmap bls_2_key --from-file=./axon-config/bls_2.key -n axon
-    kubectl create configmap bls_3_key --from-file=./axon-config/bls_3.key -n axon
-    kubectl create configmap node1-toml --from-file=./axon-config/node_1.toml -n axon
-    kubectl create configmap node2-toml --from-file=./axon-config/node_2.toml -n axon
-    kubectl create configmap node3-toml --from-file=./axon-config/node_3.toml -n axon
-    kubectl create configmap node4-toml --from-file=./axon-config/node_4.toml -n axon
-    kubectl create configmap db-options --from-file=./axon-config/default.db-options -n axon
+    kubectl create namespace axon-trial --dry-run=client -o yaml | kubectl apply -f -
+    kubectl create configmap chainspec --from-file=./axon-config/chain-spec.toml -n axon-trial
+    kubectl create configmap net-0-key --from-file=./axon-config/net_0.key -n axon-trial
+    kubectl create configmap net-1-key --from-file=./axon-config/net_1.key -n axon-trial
+    kubectl create configmap net-2-key --from-file=./axon-config/net_2.key -n axon-trial
+    kubectl create configmap net-3-key --from-file=./axon-config/net_3.key -n axon-trial
+    kubectl create configmap bls-0-key --from-file=./axon-config/bls_0.key -n axon-trial
+    kubectl create configmap bls-1-key --from-file=./axon-config/bls_1.key -n axon-trial
+    kubectl create configmap bls-2-key --from-file=./axon-config/bls_2.key -n axon-trial
+    kubectl create configmap bls-3-key --from-file=./axon-config/bls_3.key -n axon-trial
+    kubectl create configmap node1-toml --from-file=./axon-config/node_1.toml -n axon-trial
+    kubectl create configmap node2-toml --from-file=./axon-config/node_2.toml -n axon-trial
+    kubectl create configmap node3-toml --from-file=./axon-config/node_3.toml -n axon-trial
+    kubectl create configmap node4-toml --from-file=./axon-config/node_4.toml -n axon-trial
+    kubectl create configmap db-options --from-file=./axon-config/default.db-options -n axon-trial
 }
 function deploy_axon(){
     echo "DEBUG" "deploy axons, please wait..."
-    kubectl apply -f ./axon-serviceaccount.yaml
-    kubectl apply -f ./axon-1-statefulset.yaml
-    kubectl apply -f ./axon-2-statefulset.yaml
-    kubectl apply -f ./axon-3-statefulset.yaml
-    kubectl apply -f ./axon-4-statefulset.yaml
+    kubectl apply -f ./axon-serviceaccount.yaml -n axon-trial
+    kubectl apply -f ./axon1-statefulset.yaml
+    kubectl apply -f ./axon2-statefulset.yaml
+    kubectl apply -f ./axon3-statefulset.yaml
+    kubectl apply -f ./axon4-statefulset.yaml
     kubectl apply -f ./axon-chain.yaml
     kubectl apply -f ./axon-servicemonitor.yaml
     kubectl apply -f ../ingress/axon-ingress.yaml
